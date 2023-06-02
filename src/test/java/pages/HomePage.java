2e3ac81301ed241fa1f81cf1c421e150e3dd0daf
Nithin8783 <tini1038@gmail.com>
2023-06-02 09:57:36 +0530

    ds

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,14 +5,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
-
 public class HomePage{
 
     @FindBy(xpath = "//h3[contains(text(),'Notification/Circular From Head Office')]")
     private WebElement tv_notification;
 
-    @FindBy(xpath = "//span[text()='Farmerr']")
+    @FindBy(xpath = "//span[text()='Farmer']")
     private WebElement tv_farmer;
     public HomePage(WebDriver driver) {
         PageFactory.initElements(driver , this);
